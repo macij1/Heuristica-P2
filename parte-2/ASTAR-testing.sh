1f005c79fd55a5595ace486d95feb4ffe8ad7e61
macij1 <juanrmaci@gmail.com>
2023-12-15 23:38:31 +0100

    entrega

---
+++ parte-2/ASTAR-testing.sh
@@ -0,0 +1,15 @@
+python3 ASTARTraslados.py ASTAR-tests/mapa06.csv 0
+python3 ASTARTraslados.py ASTAR-tests/mapa06.csv 1
+python3 ASTARTraslados.py ASTAR-tests/mapa06.csv 4
+
+python3 ASTARTraslados.py ASTAR-tests/mapa02.csv 0
+python3 ASTARTraslados.py ASTAR-tests/mapa02.csv 1
+python3 ASTARTraslados.py ASTAR-tests/mapa02.csv 4
+
+python3 ASTARTraslados.py ASTAR-tests/mapa03.csv 0
+python3 ASTARTraslados.py ASTAR-tests/mapa03.csv 1
+python3 ASTARTraslados.py ASTAR-tests/mapa03.csv 4
+
+python3 ASTARTraslados.py ASTAR-tests/mapa09.csv 0
+python3 ASTARTraslados.py ASTAR-tests/mapa09.csv 1
+python3 ASTARTraslados.py ASTAR-tests/mapa09.csv 4
